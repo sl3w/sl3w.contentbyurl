5d8752d7435212f5fc1ab0143b746997804999cb
Leo <33494928+sl3w@users.noreply.github.com>
2022-04-15 22:54:37 +0400

    partners uri update [2]

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -3,7 +3,7 @@
 $MESS ['CONTENT_BY_URL_MODULE_NAME'] = 'Контент с привязкой по URL';
 $MESS ['CONTENT_BY_URL_MODULE_DESC'] = 'Модуль позволяет размещать контент в любом месте страницы, который привязан к конкретному URL-адресу сайта';
 $MESS ['CONTENT_BY_URL_PARTNER_NAME'] = 'SL3W';
-$MESS ['CONTENT_BY_URL_PARTNER_URI'] = 'https://github.com/sl3w/Bitrix-Module-Content-By-URL';
+$MESS ['CONTENT_BY_URL_PARTNER_URI'] = 'https://github.com/sl3w/sl3w.contentbyurl';
 
 $MESS ['CONTENT_BY_URL_INSTALL_TITLE'] = 'Установка модуля';
 $MESS ['CONTENT_BY_URL_UNINSTALL_TITLE'] = 'Деинсталляция модуля';
